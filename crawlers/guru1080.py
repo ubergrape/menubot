cd3693a4b14693fee7b210d06db629aa3815eeb9
Stefan Kröner <sk7@users.noreply.github.com>
2019-09-17 12:54:42 +0200

    convert guru1080 to proper crawler code and fix single character matching regex

--- crawlers/guru1080.py
+++ crawlers/guru1080.py
@@ -9,57 +9,59 @@ import pytesseract
 import requests
 from bs4 import BeautifulSoup
 
-
-# get image url
-
-url = "https://www.guru1080.at/wochenmen%C3%BC/"
-
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-
-img_dom = soup.select('#content_area img')
-
-if img_dom:
-    img_url = img_dom[0].attrs['src']
-else:
-    self.error_text = "Menu image not found, please check the [menu page]({})".format(url)
-    exit(0)
-
-# download image
-
-response = requests.get(img_url)
-img = Image.open(BytesIO(response.content))
-
-# upscale image because tesseract doesn't work properly with small text
-# https://stackoverflow.com/questions/4909396/is-there-any-way-to-improve-tesseract-ocr-with-small-fonts
-
-width, height = img.size
-img_resized = img.resize((width*4, height*4), Image.LANCZOS)
-img_resized.save("test.png", 'PNG')
-
-# parse text
-
-text = pytesseract.image_to_string(img_resized, lang='deu')
+from . import MenuCrawler
 
 
-# clean text
+class Guru1080Crawler(MenuCrawler):
+    name = "Guru1080"
 
-# strip stupid OCR bullshit results
-p = re.compile('[A-Z ]+\n')
-text = p.sub("",text)
+    def run(self):
+        # get image url
 
-# strip single character lines lol
-p = re.compile('.\n')
-text = p.sub("",text)
+        url = "https://www.guru1080.at/wochenmen%C3%BC/"
 
-# strip bio-vegan
-p = re.compile('.*bio-vegan.*\n')
-text = p.sub("",text)
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-# strip multiple new lines
-p = re.compile('\n+')
-text = p.sub("\n",text).strip()
+        img_dom = soup.select('#content_area img')
 
+        if img_dom:
+            img_url = img_dom[0].attrs['src']
+        else:
+            self.error_text = "Menu image not found, please check the [menu page]({})".format(url)
+            return
 
+        # download image
 
-print(text)
+        response = requests.get(img_url)
+        img = Image.open(BytesIO(response.content))
+
+        # upscale image because tesseract doesn't work properly with small text
+        # https://stackoverflow.com/questions/4909396/is-there-any-way-to-improve-tesseract-ocr-with-small-fonts
+
+        width, height = img.size
+        img_resized = img.resize((width*4, height*4), Image.LANCZOS)
+
+        # parse text
+
+        text = pytesseract.image_to_string(img_resized, lang='deu')
+
+        # clean text
+
+        # strip stupid OCR bullshit results
+        p = re.compile('[A-Z ]+\n')
+        text = p.sub("",text)
+
+        # strip single character lines lol
+        p = re.compile('^.\n', flags=re.MULTILINE)
+        text = p.sub("",text)
+
+        # strip bio-vegan
+        p = re.compile('.*bio-vegan.*\n')
+        text = p.sub("",text)
+
+        # strip multiple new lines
+        p = re.compile('\n+')
+        text = p.sub("\n",text).strip()
+
+        self.menu_text = text
