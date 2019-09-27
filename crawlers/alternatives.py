34ca803ee1a009682366c9768ebb3253d8bd0045
Stefan Kröner <sk7@users.noreply.github.com>
2019-09-27 11:51:54 +0200

    add a list of alternatives without menu parsing

---
+++ crawlers/alternatives.py
@@ -0,0 +1,17 @@
+from . import MenuCrawler
+
+class AlternativesCrawler(MenuCrawler):
+    name = "Alternatives"
+
+    def run(self):
+        self.menu_text = """
+- [**Suppenbar**](https://www.suppenbar.at/) - [Menu](https://www.suppenbar.at/wochenkarte2) - [Map](https://www.google.com/maps/dir/Lange+G.+76,+1080+Wien/Suppenbar.at,+Alser+Stra%C3%9Fe,+Wien/@48.2146496,16.3490125,17z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x476d07c03ec2cb2f:0x14980aa8b11aa668!2m2!1d16.3512013!2d48.2143976!1m5!1m1!1s0x476d07c04131af19:0x8403da8beb4d8cb0!2m2!1d16.3513401!2d48.2147882!3e2)
+
+- [**Asala Halal Food**](http://www.asala-halal-food.at/) - [Menu](http://www.asala-halal-food.at/Files/Other/1.pdf) - [Map](https://www.google.com/maps/dir/Lange+G.+76,+1080+Wien/Asala+Halal+Food,+Alser+Stra%C3%9Fe,+Wien/@48.2146496,16.3490599,17z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x476d07c03ec2cb2f:0x14980aa8b11aa668!2m2!1d16.3512013!2d48.2143976!1m5!1m1!1s0x476d07c0415d6e91:0x9a3e987498d06758!2m2!1d16.3514453!2d48.2148509!3e2)
+
+- [**Cafe Gagarin**](https://cafegagarin.at/) - [Menu](https://cafegagarin.at/warme-speisen-und-snacks-zwischendurch) - [Map](https://www.google.com/maps/dir/Lange+G.+76,+1080+Wien/Caf%C3%A9+Gagarin,+Garnisongasse+24,+1090+Wien/@48.2161727,16.3517488,17z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x476d07c03ec2cb2f:0x14980aa8b11aa668!2m2!1d16.3512013!2d48.2143976!1m5!1m1!1s0x476d07b8ac4f516f:0xdc52daa6ea8239a9!2m2!1d16.3560368!2d48.2179436!3e2)
+
+- [**Via Toledo Pizzeria**](http://viatoledo.at/) - [Menu](http://viatoledo.at/wp-content/uploads/2019/03/VIA-TOLEDO-menu-2017-3-lingue-003.pdf) - [Map](https://www.google.com/maps/dir/Lange+G.+76,+1080+Wien/Via+Toledo+Enopizzeria,+Laudongasse,+Wien/@48.213819,16.3490182,17z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x476d07c03ec2cb2f:0x14980aa8b11aa668!2m2!1d16.3512013!2d48.2143976!1m5!1m1!1s0x476d07eaa0b0c0d9:0x9d77ed166ae243fa!2m2!1d16.3513343!2d48.2132332!3e2)
+
+- **Little Lunch** - [Menu](https://www.facebook.com/pg/Little-Lunch-Wien-222373467965467/posts/?ref=page_internal) - [Map](https://www.google.com/maps/dir/Lange+G.+76,+1080+Wien/Little+Lunch,+Schl%C3%B6sselgasse,+Wien/@48.2138983,16.3500447,17z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x476d07c03ec2cb2f:0x14980aa8b11aa668!2m2!1d16.3512013!2d48.2143976!1m5!1m1!1s0x476d079568e3bd83:0xcab587b9fb9e5080!2m2!1d16.3533874!2d48.213843!3e2)
+        """
